2a60f04f75cf580650a5917805862a3afabf0999
Otthon Leão <otthonleao@hotmail.com>
2024-01-17 18:43:06 -0400

    feat: add timezone and locale config

---
+++ src/main/java/dev/otthon/parkingapi/config/SpringTimezoneConfig.java
@@ -0,0 +1,16 @@
+package dev.otthon.parkingapi.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimezoneConfig {
+
+    @PostConstruct
+    public void timezoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Manaus"));
+    }
+
+}
