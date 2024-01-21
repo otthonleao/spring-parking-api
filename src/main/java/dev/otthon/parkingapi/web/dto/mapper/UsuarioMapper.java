5b91c7cb2aa8ea52c232853bbea8f42ad4c52c91
Otthon Leão <otthonleao@hotmail.com>
2024-01-21 18:17:10 -0400

    feat: add DTO and Mapper to show just necessary in Usuario List on getAll response.

--- src/main/java/dev/otthon/parkingapi/web/dto/mapper/UsuarioMapper.java
+++ src/main/java/dev/otthon/parkingapi/web/dto/mapper/UsuarioMapper.java
@@ -6,6 +6,9 @@ import dev.otthon.parkingapi.web.dto.UsuarioResponseDTO;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UsuarioMapper {
 
     public static Usuario toUsuario(UsuarioCreateDTO createDTO) {
@@ -24,4 +27,9 @@ public class UsuarioMapper {
         mapper.addMappings(props);
         return mapper.map(usuario, UsuarioResponseDTO.class);
     }
+
+    public static List<UsuarioResponseDTO> toListDTO(List<Usuario> usuarios) {
+        return  usuarios.stream().map(user -> toDTO(user)).collect(Collectors.toList());
+    }
+
 }
