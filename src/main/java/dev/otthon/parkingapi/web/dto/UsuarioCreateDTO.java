94258f77cf57a9e5489917c7456f2fdd7585d68c
Otthon Leão <otthonleao@hotmail.com>
2024-01-24 19:28:19 -0400

    feat: add Bean Validation with `jakarta.validation.constraints`

--- src/main/java/dev/otthon/parkingapi/web/dto/UsuarioCreateDTO.java
+++ src/main/java/dev/otthon/parkingapi/web/dto/UsuarioCreateDTO.java
@@ -1,10 +1,18 @@
 package dev.otthon.parkingapi.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UsuarioCreateDTO {
 
+    @NotBlank
+    @Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "formato do e-mail está inválido")
     private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String password;
 }
