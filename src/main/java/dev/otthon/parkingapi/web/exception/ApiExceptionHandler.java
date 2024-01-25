83ce37c9d81447da2fd789eb699828723f33e1a5
Otthon Leão <otthonleao@hotmail.com>
2024-01-24 20:07:37 -0400

    feat: add MethodArgumentNotValidException for handling exceptions to validations fields

---
+++ src/main/java/dev/otthon/parkingapi/web/exception/ApiExceptionHandler.java
@@ -0,0 +1,34 @@
+package dev.otthon.parkingapi.web.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> MethodArgumentNotValidException(MethodArgumentNotValidException excecao, HttpServletRequest request, BindingResult result) {
+
+        log.error("API Error - ", excecao);
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(
+                        new ErrorMessage(
+                                request,
+                                HttpStatus.UNPROCESSABLE_ENTITY,
+                                "Campo(s) Inválido(s)",
+                                result
+                        )
+                );
+    };
+
+}
